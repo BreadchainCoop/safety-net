a71efc651c389233e9587caf94a9575f21300eb9
valeriooconte <valeriodomenicoconte@gmail.com>
2025-06-04 16:05:09 +0200

    Revert "refactor: edited interface for breadfunds"
    This reverts commit 27093d6dff7ef41b7a9e7df0ddf07a56fdb38755.

--- src/interfaces/IBreaddfonds.sol
+++ src/interfaces/IBreaddfonds.sol
@@ -1,48 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-interface IBreadfund {
+interface IBreadfonds {
   /*///////////////////////////////////////////////////////////////
                             STRUCTS
   //////////////////////////////////////////////////////////////*/
 
-  struct Request {
+  struct Breadfonds {
     address owner;
-    uint256 breadfundId;
-    uint256 timestamp;
-    string url;
-    uint256 yesVotes;
-    uint256 noVotes;
-  }
-
-  struct Breadfund {
-    address owner;
-    uint256 breadfundStart;
+    string name;
     address token;
-    address[] members;
     uint256 initialDeposit;
     uint256 fixedDeposit;
     uint256 depositInterval;
     uint256 maxWithdraws;
+    address[] members;
+    uint256[] depositAmount;
+    uint256 breadfondStart;
   }
 
   /*///////////////////////////////////////////////////////////////
                             EVENTS
   //////////////////////////////////////////////////////////////*/
 
-  event BreadfundCreated(
+  event BreadfondsCreated(
     uint256 indexed id, address[] members, address token, uint256 initialDeposit, 
     uint256 depositInterval, uint256 fixedDeposit, uint256 maxwithdraws
   );
-  event BreadfundDecommissioned(uint256 indexed id);
-  event BreadfundDeposited(uint256 indexed id, address indexed member, uint256 amount);
-  event BreadfundWithdrawn(uint256 indexed id, address indexed member, uint256 amount);
+  event BreadfondsDecommissioned(uint256 indexed id);
+  event BreadfondsDeposited(uint256 indexed id, address indexed member, uint256 amount);
+  event BreadfondsWithdrawn(uint256 indexed id, address indexed member, uint256 amount);
   event TokenAllowed(address indexed token, bool indexed allowed);
-
-  event RequestCreated(uint256 indexed id, address owner, uint256 timestamp, string url);
-  event RequestEnded(uint256 indexed id, uint256 yesVotes, uint256 noVotes);
-
-  event Voted(uint256 indexed requestId, address indexed voter, bool vote);
 
   /*///////////////////////////////////////////////////////////////
                             ERRORS
@@ -51,55 +39,53 @@ interface IBreadfund {
   error AlreadyDeposited();
   error AlreadyExists();
   error InvalidDeposit();
-  error InvalidBreadfund();
+  error InvalidBroodfonds();
   error NotCommissioned();
   error NotMember();
   error NotDecommissionable();
   error NotWithdrawable();
   error TransferFailed();
   error DepositWindowClosed();
-  error BreadfundExpired();
+  error BreadfondsExpired();
   error ExceedsDepositAmount();
-  error DepositBeforeBreadfundStart();
+  error DepositBeforeBreadfondsStart();
   error TokenNotAllowed();
   error InvalidDepositInterval();
   error InvalidDepositAmount();
-  error InvalidBreadfundStartTime();
+  error InvalidBreadfondsStartTime();
   error InvalidCurrentIndex();
   error InvalidOwner();
   error InvalidMemberCount();
   error InvalidMemberAddress();
 
-  //Breadfunds specific errors
+  //Broodfonds specific errors
   error InvalidInitialDeposit();
   error InvalidFixedDeposit();
   error InvalidMaxWithdraws();
-  error InvalidRequest();
-  error AlreadyVoted();
-  error NotAllVoted();
-  error MaxWithdrawsReached();
 
+
+// DA RIVEDERE QUESTE FUNZIONI
 
   /*///////////////////////////////////////////////////////////////
                             VIEW
   //////////////////////////////////////////////////////////////*/
 
-  function setTokenAllowed(address _token, bool _allowed) external;
-  function create(Breadfund memory breadfund) external returns (uint256);
-  function decommission(uint256 id) external;
+  function initialize(address owner) external;
+  function setTokenAllowed(address token, bool allowed) external;
+  function create(Breadfonds memory fond) external returns (uint256);
   function deposit(uint256 id, uint256 value) external;
-
-  function createRequest(Request memory request) external returns (uint256);
-  function endRequest(uint256 requestId) external;
-  function vote(uint256 requestId, bool voteValue) external;
+  function withdraw(uint256 id) external;
+  function withdrawable(uint256 id) external returns (bool);
+  function decommission(uint256 id) external;
+  function vote(uint256 id) external returns (bool);
 
   /*///////////////////////////////////////////////////////////////
                             VIEW
   //////////////////////////////////////////////////////////////*/
 
-  function getBreadfund(uint256 id) external view returns (Breadfund memory);
-  function getBreadfunds(uint256[] calldata ids) external view returns (Breadfund[] memory);
-  function getMemberBreadfunds(address member) external view returns (uint256[] memory);
+  function getBreadfonds(uint256 id) external view returns (Breadfonds memory);
+  function getBreadfonds(uint256[] calldata ids) external view returns (Breadfonds[] memory);
+  function getMemberBreadfonds(address member) external view returns (uint256[] memory);
   function getMemberBalances(uint256 id) external view returns (address[] memory, uint256[] memory);
   function checkMemberships(address member, uint256[] calldata ids) external view returns (bool[] memory);
   function isTokenAllowed(address token) external view returns (bool);
