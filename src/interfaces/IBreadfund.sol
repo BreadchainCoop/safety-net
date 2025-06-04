7f5586a94352e287903287edd8b16386665d2bf6
valeriooconte <valeriodomenicoconte@gmail.com>
2025-06-04 16:51:09 +0200

    feat: created breadfund interface

--- src/interfaces/IBreadfund.sol
+++ src/interfaces/IBreadfund.sol
@@ -2,5 +2,89 @@
 pragma solidity ^0.8.28;
 
 interface IBreadfund {
-  function initialize(address _owner) external;
+  /*///////////////////////////////////////////////////////////////
+                            STRUCTS
+  //////////////////////////////////////////////////////////////*/
+  struct Breadfund {
+    address owner;
+    uint256 breadfundStart;
+    address token;
+    address[] members;
+    uint256 initialDeposit;
+    uint256 fixedDeposit;
+    uint256 depositInterval;
+    uint256 maxWithdraws;
+  }
+
+  /*///////////////////////////////////////////////////////////////
+                            EVENTS
+  //////////////////////////////////////////////////////////////*/
+
+  event BreadfundCreated(
+    uint256 indexed id,
+    address[] members,
+    address token,
+    uint256 initialDeposit,
+    uint256 depositInterval,
+    uint256 fixedDeposit,
+    uint256 maxwithdraws
+  );
+  event BreadfundDecommissioned(uint256 indexed id);
+  event BreadfundDeposited(uint256 indexed id, address indexed member, uint256 amount);
+  event BreadfundWithdrawn(uint256 indexed id, address indexed member, uint256 amount);
+  event TokenAllowed(address indexed token, bool indexed allowed);
+  event NewBreadfundMember(uint256 indexed id, address indexed member, uint256 amount);
+
+  /*///////////////////////////////////////////////////////////////
+                            ERRORS
+  //////////////////////////////////////////////////////////////*/
+
+  error AlreadyDeposited();
+  error AlreadyExists();
+  error InvalidDeposit();
+  error InvalidBreadfund();
+  error NotCommissioned();
+  error NotMember();
+  error NotDecommissionable();
+  error NotWithdrawable();
+  error TransferFailed();
+  error DepositWindowClosed();
+  error BreadfundExpired();
+  error ExceedsDepositAmount();
+  error DepositBeforeBreadfundStart();
+  error TokenNotAllowed();
+  error InvalidDepositInterval();
+  error InvalidDepositAmount();
+  error InvalidBreadfundStartTime();
+  error InvalidCurrentIndex();
+  error InvalidOwner();
+  error InvalidMemberCount();
+  error InvalidMemberAddress();
+
+  //Breadfunds specific errors
+  error InvalidInitialDeposit();
+  error InvalidFixedDeposit();
+  error InvalidMaxWithdraws();
+  error MaxWithdrawsReached();
+
+  /*///////////////////////////////////////////////////////////////
+                            EXTERNAL
+  //////////////////////////////////////////////////////////////*/
+
+  function initialize(address owner) external;
+  function setTokenAllowed(address token, bool allowed) external;
+  function create(Breadfund memory breadfund) external returns (uint256);
+  function decommission(uint256 id) external;
+  function register(uint256 id, uint256 contribute) external;
+  function deposit(uint256 id, uint256 value) external;
+
+  /*///////////////////////////////////////////////////////////////
+                            VIEW
+  //////////////////////////////////////////////////////////////*/
+
+  function getBreadfund(uint256 id) external view returns (Breadfund memory);
+  function getBreadfunds(uint256[] calldata ids) external view returns (Breadfund[] memory);
+  function getMemberBreadfunds(address member) external view returns (uint256[] memory);
+  function getMemberBalances(uint256 id) external view returns (address[] memory members, uint256[] memory balances);
+  function isTokenAllowed(address token) external view returns (bool);
 }
